bd357e6c74defbbb91a08dcc454ddc704acbf47a
BRRAMY004-AmyBarry <brramy004@myuct.ac.za>
2025-05-22 09:20:06 +0000

    edits

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,19 @@
 import streamlit as st
 
-st.title("🎈 My new StreamLit app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("Village Health Laundry")
+st.header("About Us")
+
+#name = st.text_input("Enter your name")
+
+#studymethod = st.selectbox("Study Method", ("Crammer","Pre-planner","Memory Box"))
+
+#button = st.button("Diva?")
+
+#if button:
+#    with st.spinner("Loading your diva status"):
+#        if studymethod == "Crammer":
+#           st.write(F"{name}, You are NOT a diva")
+#        elif studymethod == "Pre-planner":
+#           st.write(F"{name}, You are a diva")
+#        elif studymethod == "Memory Box":
+#           st.write(F"{name}, You are almost a diva")
